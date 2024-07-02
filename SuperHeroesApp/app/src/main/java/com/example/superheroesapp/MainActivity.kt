d7e31d029b2189ab426b72335876407130042188
Haseeb-U <171488663+Haseeb-U@users.noreply.github.com>
2024-07-02 12:45:42 -0700

    P14:- Super Heroes App

--- SuperHeroesApp/app/src/main/java/com/example/superheroesapp/MainActivity.kt
+++ SuperHeroesApp/app/src/main/java/com/example/superheroesapp/MainActivity.kt
@@ -15,6 +15,8 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import com.example.superheroesapp.ui.theme.SuperHeroesAppTheme
 
 class MainActivity : ComponentActivity() {
